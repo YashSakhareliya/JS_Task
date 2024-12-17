aedc50aa3da9d35346e74d7ed8293636d7e1df0c
Yash sakhareliya <sakhareliyayash@gmail.com>
2024-12-17 13:01:30 +0530

    Added Task 1

---
+++ Task 1/index.js
@@ -0,0 +1,10 @@
+let name = 'Yash Sakhareliya'
+let age = 19
+let email = 'sakhareliyayash@gmail.com'
+let phone = '123-456-897'
+
+document.getElementById('title').textContent = 'Resume'
+document.getElementById('name').textContent = `${name}`
+document.getElementById('about').textContent = 'About'
+document.getElementById('about-content').textContent = 'I am a highly motivated and dedicated B.Tech student at \nParul University, specializing in Information Technology. \nCurrently in my third year, I am building a strong foundation in Data Structures and Algorithms (DSA) while actively learning Data Science, \nBig Data, and Data Mining. I also hold certifications \nin Data Science and Computer Network and Internet Protocol.'
+document.getElementById('age').textContent = `AGE :${age}`
